ec6cb77d3dcbb9865cad420c9b88860acda58043
adrianld <adrianlopezdacosta@gmail.com>
2015-02-04 11:37:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/adrian/intents/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.adrian.intents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button botonWeb;
+    Button botonEmail;
+    Button botonTelefono;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        botonWeb=(Button)findViewById(R.id.botonWeb);
+        botonEmail=(Button)findViewById(R.id.botonEmail);
+        botonTelefono=(Button)findViewById(R.id.botonTelefono);
+
+        botonWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent miIntent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.lavozdegalicia.es"));
+                startActivity(miIntent);
+            }
+        });
+
+        botonTelefono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent miIntent=new Intent(Intent.ACTION_CALL,Uri.parse("tel:636671866"));
+                startActivity(miIntent);
+            }
+        });
+
+        botonEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent miIntent=new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:adrianlopezdacosta@gmail.com"));
+                startActivity(miIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+            }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
